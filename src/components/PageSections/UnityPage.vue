e870eafa659c20abd5a4afca5e0f7a4f381fc647
RMSpratt <reedspratt4@gmail.com>
2024-05-24 14:24:04 -0400

    Add Unity Page
    - Upload Unity Page Component
    - Upload Unity JSON Content
    - Upload Unity Project Images

---
+++ src/components/PageSections/UnityPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import SectionBlock from './PageContent/SectionBlock.vue'
+
+import UnityMain from '../../assets/Unity/UnityMain.json'
+</script>
+
+<template>
+  <div v-for="(section, index) in UnityMain" v-bind:key="index" class="page-section-row">
+    <div class="page-section-center">
+      <SectionBlock :section-body="section" />
+    </div>
+  </div>
+</template>
+<style lang="scss"></style>
