0f41ffadecb12b4cc9b1c003b94d2cf5e16c5b50
RMSpratt <reedspratt4@gmail.com>
2024-05-30 17:50:14 -0400

    Set Base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,9 +13,10 @@ export default defineConfig({
   },
   css: {
     preprocessorOptions: {
-        scss: {
-            additionalData: `@import "@/styles/_variables.scss";`
-        }
+      scss: {
+        additionalData: `@import "@/styles/_variables.scss";`
+      }
     }
-  }
+  },
+  base: '/Portfolio-Website'
 })
